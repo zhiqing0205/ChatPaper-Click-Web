1e1d26cfcc40e36e77da1e5d09b722332c0be97b
zzq1775840762 <1775840762@qq.com>
2023-12-26 17:18:40 +0800

    引入elementui

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <el-button type="primary">Hello World</el-button>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
