364cea7d6e738b8faef08a54eb862ba49c449dd1
zzq1775840762 <1775840762@qq.com>
2023-12-27 18:04:46 +0800

    完成头部的编写

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <!--显示logo和slogan-->
+    <div class="header">
+        <img src="@/assets/ChatGPT.png" alt="logo" @click="to_index"/>
+        <span @click="to_index">ChatPaper</span>
+        <!-- 显示在最右边 -->
+        <img src="@/assets/GitHub.png" alt="github" style="margin-left: auto; margin-right: 25px;" @click="to_github"/>
+      </div>  
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  methods: {
+    to_index() {
+      this.$router.push({ path: '/' });
+    },
+    to_github() {
+      window.open('https://github.com/zhiqing0205/ChatPaper-Click');
+    }
+  }
+}
+</script>
+
+<style>
+.header{
+  display: flex;
+  /* 左对齐 */
+  justify-content: flex-start;
+  align-items: center;
+  padding-left: 25px;
+  font-size: 30px;
+  font-weight: bold;
+  color: #409eff;
+  /* background-color: #d3e3fd; */
+  img{
+    width: 36px;
+    height: 36px;
+    margin-right: 6px;
+  }
+}
+</style>
