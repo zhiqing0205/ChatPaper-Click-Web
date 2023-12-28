251a0974e826cf72f271a383b9eb2105aabf641c
zzq1775840762 <1775840762@qq.com>
2023-12-29 01:39:39 +0800

    主页功能完善

--- src/views/index.vue
+++ src/views/index.vue
@@ -4,8 +4,12 @@
         <el-upload
             class="upload-demo"
             drag
-            action="https://jsonplaceholder.typicode.com/posts/"
             style="margin-bottom: 36px; margin-top: 25px;"
+            accept=".pdf"
+            action="https://api.chatpaper.click/upload"
+            :before-upload="beforeUpload"
+            :on-success="uploadSuccess"
+            :on-error="uploadError"
             >
             <i class="el-icon-upload"></i>
             <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
@@ -14,14 +18,14 @@
         
         <div style="margin-top: 15px;">
             <el-input placeholder="请输入PDF链接" v-model="input2">
-                <template slot="append"><el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button></template>
+                <template slot="append"><el-button type="primary" @click="submit">上传<i class="el-icon-upload el-icon--right"></i></el-button></template>
             </el-input>
         </div>
 
         <el-divider>最近动态</el-divider>    
         <el-carousel :interval="5000" arrow="always" v-if="papers.length > 0">
-            <el-carousel-item v-for="paper in papers" :key="paper.id" :style="backgroundImageStyle(n)">
-                <div class="mask">
+            <el-carousel-item v-for="paper in papers" :key="paper.id" :style="backgroundImageStyle(paper.id)">
+                <div class="mask"  @click="detail(paper.path)">
                     <h2>{{ paper.title }} - {{ paper.created_at }}</h2>
                 </div>
             </el-carousel-item>
@@ -30,7 +34,7 @@
 </template>
 
 <script>
-import { getRecentPapers } from '@/api';
+import { getRecentPapers, uploadPaper } from '@/api';
 export default{
     name:'',
     data(){
@@ -45,12 +49,56 @@ export default{
                 this.papers = res;
             });
         },
-        backgroundImageStyle(n) {
-            const randomNum = Math.random(); // Generates a random number
-            const url = `https://img.ziuch.top/i/random.php?random=${randomNum}`;
+        backgroundImageStyle(id) {
+            const url = `https://img.ziuch.top/i/random.php?random=${id}`;
             return {
                 backgroundImage: `url(${url})`,
             };
+        },
+        detail(path){
+            this.$router.push({path: '/analysis', query: {pdf_url: path}});
+        },
+        submit() {
+            if(this.input2 === ''){
+                this.$message({
+                    message: '链接不能为空',
+                    type: 'error'
+                });
+            } else {
+                this.$message({
+                    message: '上传成功',
+                    type: 'success'
+                });
+
+                this.$router.push({path: '/analysis', query: {pdf_url: this.input2}});
+            }
+        },
+        beforeUpload(file) {
+            const isPDF = file.type === 'application/pdf';
+            const isLt10M = file.size / 1024 / 1024 < 10;
+
+            if (!isPDF) {
+                this.$message.error('上传文件只能是 PDF 格式!');
+            }
+            if (!isLt10M) {
+                this.$message.error('上传文件大小不能超过 10MB!');
+            }
+            return isPDF && isLt10M;
+        },
+        uploadSuccess(response, file, fileList){
+            this.$message({
+                message: '上传成功',
+                type: 'success'
+            });
+            // 从static开始
+            let path = 'static/' + response.url.split('static/')[1];
+            this.$router.push({path: '/analysis', query: {pdf_url: path}});
+        },
+        uploadError(err, file, fileList){
+            this.$message({
+                message: '上传失败',
+                type: 'error'
+            });
         }
     },
     mounted(){
